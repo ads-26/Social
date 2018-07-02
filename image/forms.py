f485d5dd9c24952318244b30f84fa62687b678ac
ads-26 <40528745+ads-26@users.noreply.github.com>
2018-07-02 11:54:29 +0530

    Update forms.py

--- image/forms.py
+++ image/forms.py
@@ -8,30 +8,4 @@ class ImageCreateForm(forms.ModelForm):
 	class Meta:
 		model=Image
 		fields=['title','img','description']
-	"""
-		widgets={
-		'url':forms.HiddenInput,
-		}
 	
-    
-	def clean_url(self):
-		url=self.cleaned_data['url']
-		valid_extensions=['jpg','jpeg','png']
-		#extension=url.rsplit('.',1).lower()
-		extension=url.rsplit('.',1)
-		if extension not in valid_extensions:
-			raise forms.ValidationError('The given link does not give valid image extensions.')
-		return url
-
-	def save(self, force_insert=False,force_update=False,commit=True):    
-		image = super(ImageCreateForm, self).save(commit=False)    
-		#img_url = self.cleaned_data['url']    
-		#img_name = '{}.{}'.format(slugify(image.title),img_url.rsplit('.', 1)[1].lower())
-		img_name = '{}'.format(slugify(image.title))  
-		response = request.open(image)    
-		image.image.save(img_name,ContentFile(response.read()),save=False)    
-		if commit:        
-			image.save()    
-			return image
-
-	"""
