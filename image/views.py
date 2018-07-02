7ae3e49b6a8dd1dfe619d7f73a22865562254fd5
ads-26 <40528745+ads-26@users.noreply.github.com>
2018-07-02 11:56:50 +0530

    Update views.py

--- image/views.py
+++ image/views.py
@@ -11,19 +11,12 @@ def image_create(request):
 		form = ImageCreateForm(data=request.POST,files=request.FILES)        
 		if form.is_valid():        
 			cd = form.cleaned_data
-			"""
-			m=Image.objects.get(pk=Image.id) 
-			m.img=cd['image']
-			m.save()
-			"""          
+			      
 			new_item = form.save(commit=False)            
 			new_item.user = request.user            
 			new_item.save()         
-			messages.success(request, 'Image added successfully')            
-			#return redirect(new_item.get_absolute_url())    
+			messages.success(request, 'Image added successfully')                
 	else:
 		form = ImageCreateForm(data=request.GET)
 
 	return render(request,'image/img/create.html',{'section': 'images','form': form})
-
-# Create your views here.
